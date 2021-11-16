3506eb37c2a564f32db5a42811dc4d2fa3bdbff7
rubengarsan <94488111+rubengarsan@users.noreply.github.com>
2021-11-16 13:52:49 -0700

    Add files via upload

---
+++ math solving.py
@@ -0,0 +1,37 @@
+import math
+
+a = int(input("enter a:"))
+b = int(input('Enter b:'))
+c = int(input("enter c:"))
+a != 0
+q = (b*b)
+e = (4*a*c)
+def quadratic_function(a,b,c):
+    d = q - e
+    sqrtd = math.sqrt(int(d))
+    global x1
+    x1 = (-b-sqrtd)/(2*a)
+    global x2
+    x2 = (-b+sqrtd)/(2*a)
+def imaginary(a,b,c):
+    f = e - q
+    sqrtd = math.sqrt(int(f))
+    global x1
+    x1 = (-b-sqrtd)/(2*a)
+    global x2
+    x2 = (-b+sqrtd)/(2*a)
+
+while True:
+    if a == 0:
+        print("a cannot be 0")
+    elif q > e:
+        quadratic_function(a,b,c)
+        print(f'The solution are {x1} and {x2}')
+    elif e > q:
+        imaginary(a,b,c)
+        print(f'The solution are {x1}i and {x2}i')
+    elif q == e:
+        print(-b/2*a)
+    else:
+        print("This quadratic equation cannot be done")
+    a = input()

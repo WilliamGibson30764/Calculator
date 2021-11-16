cd8ca17e17cb3141c01cce6dd5df4a4d2d4f12d3
rubengarsan <94488111+rubengarsan@users.noreply.github.com>
2021-11-16 13:57:05 -0700

    Add files via upload

--- math solving.py
+++ math solving.py
@@ -6,6 +6,23 @@ c = int(input("enter c:"))
 a != 0
 q = (b*b)
 e = (4*a*c)
+def code():
+    q = (b*b)
+    e = (4*a*c)
+    while True:
+    if a == 0:
+        print("a cannot be 0")
+    elif q > e:
+        quadratic_function(a,b,c)
+        print(f'The x intercepts are {x1} and {x2}')
+    elif e > q:
+        imaginary(a,b,c)
+        print(f'The x  are {x1}i and {x2}i')
+    elif q == e:
+        print(-b/2*a)
+    else:
+        print("This quadratic equation cannot be done")
+    a = input()
 def quadratic_function(a,b,c):
     d = q - e
     sqrtd = math.sqrt(int(d))
@@ -26,10 +43,10 @@ while True:
         print("a cannot be 0")
     elif q > e:
         quadratic_function(a,b,c)
-        print(f'The solution are {x1} and {x2}')
+        print(f'The x intercepts are {x1} and {x2}')
     elif e > q:
         imaginary(a,b,c)
-        print(f'The solution are {x1}i and {x2}i')
+        print(f'The x  are {x1}i and {x2}i')
     elif q == e:
         print(-b/2*a)
     else:
